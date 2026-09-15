b7aeda4029755cbdb40c180fa8ec260532be0bec
Northy151-91 <jonathon.south91@gmail.com>
2026-09-15 17:26:37 +1000

    Fix app rendering and caching
    Fixes blank Crafts/More screens caused by the horse stats update and improves service worker cache refresh behaviour.

--- sw.js
+++ sw.js
@@ -1,3 +1,31 @@
-const CACHE='rdr2-field-ledger-v12';const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='rdr2-field-ledger-v13';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put('./index.html',copy));
+        return res;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy));
+      return res;
+    }))
+  );
+});
