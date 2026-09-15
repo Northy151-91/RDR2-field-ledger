0fcb3c25117f521ad13f42030d8bc3a7d9a3fc5e
Northy151-91 <jonathon.south91@gmail.com>
2026-09-15 17:13:22 +1000

    Add initial app files
    Adds the initial RDR2 Field Ledger web app, PWA manifest, and offline service worker.

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='rdr2-field-ledger-v12';const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
